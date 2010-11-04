d0a24ff8d5f9be992586f236db60e352b9f556a1
Joris <jorismorger@gmail.com>
2010-11-04 20:02:53 +0100

    chive hinzugefügt (alternative zu phpMyAdmin, unter root/chive erreichbar

---
+++ chive/index.php
@@ -0,0 +1,76 @@
+<?php
+
+// Some dirty debugging methods
+function pre($_value) { echo "<pre>"; print_r($_value); echo "</pre>"; }
+function predie($_value) { pre($_value); Yii::app()->end(); }
+
+// Yii debug mode
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+
+// Load Yii
+require('yii/yii.php');
+
+// Create web application
+$app = Yii::createWebApplication('protected/config/main.php');
+
+// Define constants
+define('BASEURL', Yii::app()->baseUrl);
+define('ICONPATH', BASEURL . '/images/icons/' . Yii::app()->params->iconPack);
+
+$validPaths = array(
+	'site',
+	'index.php',
+);
+
+if(!$app->user->isGuest)
+{
+	$app->db->connectionString = 'mysql:host=' . $app->user->host . ';dbname=information_schema';
+	$app->db->username= $app->user->name;
+    $app->db->password= $app->user->password;
+    $app->db->autoConnect = true;
+    $app->db->setActive(true);
+}
+elseif(!preg_match('/^(' . implode('|', $validPaths) . ')/i', Yii::app()->urlManager->parseUrl($app->request)))
+{
+	if($app->request->isAjaxRequest)
+	{
+		$response = new AjaxResponse();
+		$response->redirectUrl = Yii::app()->createUrl('site/login');
+		$response->send();
+	}
+	else
+	{
+		$app->request->redirect(Yii::app()->createUrl('site/login'));
+	}
+}
+
+// Language
+if($app->session->itemAt('language'))
+{
+	$app->setLanguage($app->session->itemAt('language'));
+}
+elseif($app->request->getPreferredLanguage() && is_dir('protected/messages/' . $app->request->getPreferredLanguage()))
+{
+	$app->setLanguage($app->request->getPreferredLanguage());
+}
+else
+{
+	$app->setLanguage('en_us');
+}
+
+// Theme
+$theme = $app->session->itemAt('theme') ? $app->session->itemAt('theme') : 'standard';
+$app->setTheme($theme);
+
+// Unset jQuery in Ajax requests
+if($app->request->isAjaxRequest)
+{
+	$app->clientScript->scriptMap['jquery.js'] = false;
+	$app->clientScript->scriptMap['jquery.min.js'] = false;
+}
+
+// Publish messages for javascript usage
+Yii::app()->getComponent('messages')->publishJavaScriptMessages();
+
+// Run application
+$app->run();
---
+++ chive/protected/runtime/views/b1681807/index.php
@@ -0,0 +1,9 @@
+<?php /* source file: /Users/jorismorger/longboardspots/chive/protected/views/schema/index.php */ ?>
+<script type="text/javascript">
+$(document).ready(function() {
+	if(location.href.indexOf('#') == -1)
+	{
+		chive.goto('tables');
+	}
+});
+</script>
