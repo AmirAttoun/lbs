1f4d87ee8094ab9c2bd808f883fe76521c79ccab
Amir <attoun.amir@gmail.com>
2010-11-11 13:40:17 +0100

    Ich bin ein Committest

--- application/language/english/start_lang.php
+++ application/language/english/start_lang.php
@@ -1,8 +1,6 @@
 <?php
 
-$lang['start.hello'] = "Hello, this is the english Page!";
+$lang['start.hello'] = "Hello, this is the english Page! ich liebe dich";
 $lang['start.enter'] = "Click here to enter.";
-
-
 
 ?>
